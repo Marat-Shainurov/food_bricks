4b6aba06182d87dc48390433a4babe6d72123625
Marat-Shainurov <m_shainurov@mail.ru>
2024-08-20 17:15:16 +0400

    upd wrapper with BottomNavigationBar

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:food_bricks/screens/plan/plan_home.dart';
 import 'package:food_bricks/screens/home/constructors_home.dart';
 
-class Wrapper extends StatelessWidget {
-  const Wrapper({super.key});
+class Wrapper extends StatefulWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  _WrapperState createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  int _selectedIndex = 0;
+
+  // List of widgets to display for each tab
+  static const List<Widget> _widgetOptions = <Widget>[
+    Plan(), // Planner widget
+    constructorsHome(), // Constructors widget
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const constructorsHome();
+    return Scaffold(
+      body: _widgetOptions[_selectedIndex], // Display the selected widget
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Planner',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Constructors',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue[500],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
