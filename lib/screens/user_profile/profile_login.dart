1fdaffc1b3f58fc04ef8d256f370ab918eb03a55
Marat-Shainurov <m_shainurov@mail.ru>
2024-09-03 11:37:59 +0400

    add _showDietPopup and showDialog, upd profile widget

--- lib/screens/user_profile/profile_login.dart
+++ lib/screens/user_profile/profile_login.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:food_bricks/services/auth_service.dart';
-import 'authenticate.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:telephony/telephony.dart';
+import 'package:food_bricks/services/odoo_service.dart';
 
 class UserProfile extends StatefulWidget {
   final String? userPhone;
   final Function(String) setUserPhone;
+  final Function(Map) setClientData;
+  final Map<dynamic, dynamic>? clientData;
+  final OdooService odooService;
 
-  const UserProfile({Key? key, this.userPhone, required this.setUserPhone})
+  const UserProfile(
+      {Key? key,
+      this.userPhone,
+      required this.setUserPhone,
+      required this.setClientData,
+      this.clientData,
+      required this.odooService})
       : super(key: key);
 
   @override
@@ -23,6 +31,10 @@ class _UserProfileState extends State<UserProfile> {
 
   final _formKey = GlobalKey<FormState>();
   final _formKey1 = GlobalKey<FormState>();
+
+  dynamic sessionId = '';
+  Map clientDataOdoo = {};
+  List<dynamic> availableDiets = [];
 
   void listenToIncomingSMS(BuildContext context) {
     print("Listening to sms.");
@@ -52,6 +64,8 @@ class _UserProfileState extends State<UserProfile> {
       AuthService.loginWithOtp(otp: _otpController.text).then((value) {
         if (value == "Success") {
           widget.setUserPhone(_phoneController.text);
+          _fetchAndSetOdooClient();
+          print(widget.clientData);
           Navigator.pop(context);
         } else {
           Navigator.pop(context);
@@ -67,16 +81,139 @@ class _UserProfileState extends State<UserProfile> {
     }
   }
 
+  Future<void> _fetchOdooSession() async {
+    try {
+      sessionId = await widget.odooService.fetchSessionId();
+      print('Fetched session id: $sessionId');
+    } catch (e) {
+      print('Error fetching: $e');
+    }
+  }
+
+  Future<void> _fetchAndSetOdooClient() async {
+    try {
+      clientDataOdoo = await widget.odooService
+          .getOrCreateOdooClient(sessionId, _phoneController.text);
+      print('Fetched client: $clientDataOdoo');
+      widget.setClientData(clientDataOdoo);
+    } catch (e) {
+      print('Error fetching client: $e');
+    }
+  }
+
+  Future<void> _fetchAvailableDiets() async {
+    try {
+      availableDiets =
+          await widget.odooService.fetchDiets(sessionId, _phoneController.text);
+      setState(() {});
+    } catch (e) {
+      print('Error fetching available diets: $e');
+    }
+  }
+
   @override
   void initState() {
     super.initState();
+    _fetchOdooSession();
+    _fetchAvailableDiets();
     if (widget.userPhone != null) {
       _phoneController.text = widget.userPhone!;
     }
   }
 
+  void _showDietPopup(BuildContext context) async {
+    if (availableDiets.isEmpty) {
+      await _fetchAvailableDiets();
+    }
+
+    final currentDiets = widget.clientData?['diets'] ?? [];
+    final selectedDiets = Set<String>.from(currentDiets);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Edit Dietary Preferences'),
+              content: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Current Diets:'),
+                    ...currentDiets.map((diet) {
+                      return CheckboxListTile(
+                        title: Text(diet),
+                        value: selectedDiets.contains(diet),
+                        onChanged: (bool? value) {
+                          setState(() {
+                            if (value == true) {
+                              selectedDiets.add(diet);
+                            } else {
+                              selectedDiets.remove(diet);
+                            }
+                          });
+                        },
+                      );
+                    }).toList(),
+                    SizedBox(height: 20),
+                    Text('Available Diets:'),
+                    ...availableDiets.map((diet) {
+                      return CheckboxListTile(
+                        title: Text(diet),
+                        value: selectedDiets.contains(diet),
+                        onChanged: (bool? value) {
+                          setState(() {
+                            if (value == true) {
+                              selectedDiets.add(diet);
+                            } else {
+                              selectedDiets.remove(diet);
+                            }
+                          });
+                        },
+                      );
+                    }).toList(),
+                  ],
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle the Apply logic
+                    print('currentDiets - ${currentDiets}');
+                    print('selectedDiets - ${selectedDiets}');
+                    Navigator.pop(context, selectedDiets);
+                  },
+                  child: Text('Apply'),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    ).then((selectedDiets) {
+      if (selectedDiets != null) {
+        setState(() {
+          widget.clientData?['diets'] = selectedDiets.toList();
+          // Send the updated diets back to the server here if needed.
+          print('currentDiets - ${currentDiets}');
+          print('selectedDiets - ${selectedDiets}');
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final clientName = widget.clientData?['name'] ?? 'User';
+    final diets = widget.clientData?['diets'] ?? [];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -191,12 +328,55 @@ class _UserProfileState extends State<UserProfile> {
                     )
                   ],
                 )
-              : Center(
-                  child: Text(
-                    'Welcome, ${widget.userPhone}',
-                    style: const TextStyle(
-                        fontSize: 18.0, fontWeight: FontWeight.bold),
-                  ),
+              : Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    Card(
+                      elevation: 4,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Dietary Preferences',
+                              style: TextStyle(
+                                  fontSize: 16.0, fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 30),
+                            diets.isEmpty
+                                ? ElevatedButton(
+                                    onPressed: () => _showDietPopup(context),
+                                    child: const Text('Add'),
+                                  )
+                                : Column(
+                                    children: [
+                                      ConstrainedBox(
+                                        constraints:
+                                            BoxConstraints(maxHeight: 200),
+                                        child: ListView.builder(
+                                          shrinkWrap: true,
+                                          itemCount: diets.length,
+                                          itemBuilder: (context, index) {
+                                            return Text('- ${diets[index]}');
+                                          },
+                                        ),
+                                      ),
+                                      Align(
+                                        alignment: Alignment.bottomRight,
+                                        child: ElevatedButton(
+                                          onPressed: () =>
+                                              _showDietPopup(context),
+                                          child: const Text('Edit'),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
         ),
       ),
